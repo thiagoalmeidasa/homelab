245246f517ea5e7ce3762e7e649f51a1424f2261
Thiago Almeida <thiagoalmeidasa@gmail.com>
2025-02-16 17:43:30 -0300

    feat(cloud-backups): added a b2 bucket for nas backups

---
+++ terraform/cloud-backups/provider.tf
@@ -0,0 +1,4 @@
+provider "b2" {
+  application_key    = var.b2_application_key
+  application_key_id = var.b2_application_key_id
+}
---
+++ terraform/cloud-backups/variables.tf
@@ -0,0 +1,7 @@
+variable "b2_application_key" {
+  type = string
+}
+
+variable "b2_application_key_id" {
+  type = string
+}
