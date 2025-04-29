0658de4e39733de9a691712e8bc510fdb184c792
Thiago Almeida <thiagoalmeidasa@gmail.com>
2025-04-29 07:21:40 +0200

    feat(storage): add kopia as backup solution (#1643)
    This commit introduces a kopia-based backup solution for the NAS.
    
    Key changes:
    
    - Added a new Kustomization for kopia in `kubernetes/apps/storage/kopia`.
    - Defined a HelmRelease for kopia deployment.
    - Created necessary secrets for kopia configuration (repository config and password).
    - Created a new B2 bucket for kopia backups.
    - Configured the Kustomization to deploy kopia to the storage namespace.
    
    This setup allows for efficient and secure backups of the NAS data to Backblaze B2.

--- terraform/cloud-backups/nas-backup-bucket.tf
+++ terraform/cloud-backups/nas-backup-bucket.tf
@@ -8,3 +8,12 @@ resource "b2_bucket" "nas-backup-bucket" {
   }
 }
 
+resource "b2_bucket" "kopia-nas-backup-bucket" {
+  bucket_name = "kopia-home399-nas-backup-bucket"
+  bucket_type = "allPrivate"
+
+  default_server_side_encryption {
+    algorithm = "AES256"
+    mode      = "SSE-B2"
+  }
+}
