504291a54a15f2d7662e2ff3ea49538518a946de
Thiago Almeida <thiagoalmeidasa@gmail.com>
2026-05-16 21:18:21 +0200

    fix(terraform): match RustFS canonical IAM policy form

--- terraform/local-s3-buckets/mimir.tf
+++ terraform/local-s3-buckets/mimir.tf
@@ -12,20 +12,23 @@ resource "minio_iam_user" "mimir" {
 resource "minio_iam_policy" "mimir" {
   name = "mimir-policy"
   policy = jsonencode({
+    ID      = ""
     Version = "2012-10-17"
     Statement = [{
+      Sid    = ""
       Effect = "Allow"
       Action = [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject",
         "s3:ListBucket",
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject",
         "s3:GetBucketLocation",
       ]
       Resource = [
-        "arn:aws:s3:::mimir",
         "arn:aws:s3:::mimir/*",
+        "arn:aws:s3:::mimir",
       ]
+      Condition = {}
     }]
   })
 }
